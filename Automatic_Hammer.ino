f9f620e6f9969da1313463eadb3ba7ed177ae627
Jin <56622731+taurusx78@users.noreply.github.com>
2021-09-03 18:57:08 +0900

    Add files via upload
    .aia : 앱 인벤터 소스 코드 파일
    .apk : 안드로이드용 프로그램 설치 파일
    .ino : 아두이노 소스 코드 파일

---
+++ Automatic_Hammer.ino
@@ -0,0 +1,51 @@
+#define BUTTON1 A1
+#define BUZZER_DIO 3
+#define LED1_DIO 10
+#define LED2_DIO 11
+#define LED3_DIO 12
+#define LED4_DIO 13
+
+#define ON LOW
+#define OFF HIGH
+
+void setup() {
+  pinMode(BUTTON1, INPUT);
+  pinMode(BUZZER_DIO, OUTPUT);
+  pinMode(LED1_DIO, OUTPUT);
+  pinMode(LED2_DIO, OUTPUT);
+  pinMode(LED3_DIO, OUTPUT);
+  pinMode(LED4_DIO, OUTPUT);
+
+  digitalWrite(BUZZER_DIO, OFF);
+  digitalWrite(LED1_DIO, OFF);
+  digitalWrite(LED2_DIO, OFF);
+  digitalWrite(LED3_DIO, OFF);
+  digitalWrite(LED4_DIO, OFF);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  char data = 0;
+  if (Serial.available() > 0) {
+    char value = Serial.read();
+    if (value == 2) {
+      digitalWrite(BUZZER_DIO, OFF);
+      digitalWrite(LED1_DIO, OFF);
+      digitalWrite(LED2_DIO, OFF);
+      digitalWrite(LED3_DIO, OFF);
+      digitalWrite(LED4_DIO, OFF);
+    }
+  }
+  while (digitalRead(BUTTON1) == 0) {
+    data = 0b0001;
+    digitalWrite(BUZZER_DIO, ON);
+      digitalWrite(LED1_DIO, ON);
+      digitalWrite(LED2_DIO, ON);
+      digitalWrite(LED3_DIO, ON);
+      digitalWrite(LED4_DIO, ON);
+  }
+  if (data > 0) {
+    Serial.write(data);
+    }
+}
